cb751a29dd85d172bc15366c1690bab91c10a13e
biubiu <549303468@qq.com>
2024-04-07 17:16:34 +0800

    Update env_setup.sh

--- env_setup.sh
+++ env_setup.sh
@@ -4,4 +4,5 @@ mkdir -p /app/tensorrt_llm
 export HF_MODULE_CACHE="/app/tensorrt_llm/"
 export TRANSFORMERS_CACHE="/app/tensorrt_llm/"
 export XDG_CACHE_HOME="/app/tensorrt_llm/"
-export CUDA_VISIBLE_DEVICES=0                                   
+export CUDA_VISIBLE_DEVICES=0   
+export HF_ENDPOINT=https://hf-mirror.com # Or use HF_ENDPOINT=https://hf-mirror.com python your_script.py
